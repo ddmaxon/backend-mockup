260a26f8ae15682ddf8f70c419be5a0454f3b7e9
Netshlife <levinzimmermann06@gmail.com>
2024-02-07 08:45:09 +0100

    Initial commit

---
+++ testapi/CsvLoader.cs
@@ -0,0 +1,127 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System.IO;
+
+
+namespace testapi
+{
+    public class CsvLoader
+    {
+        private List<string> _csv;
+
+        public CsvLoader(string csvPath = "./")
+        {
+            this._csv = new List<string>();
+            this.loadCsv(csvPath);
+        }
+
+        public void loadCsv(string path)
+        {
+            using (var reader = new StreamReader($"{path}"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var val = reader.ReadLine();
+
+                    if (val == null)
+                        continue;
+
+                    this._csv.Add(val);
+                }
+            }
+        }
+
+        public List<string> GetCsvLines()
+        {
+            return _csv;
+        }
+
+        public string GetCsvLine(int lineNr)
+        {
+            try
+            {
+                return this._csv[(lineNr - 1)];
+            }
+            catch (Exception err)
+            {
+                throw new Exception($"Out of range! Max range is {this._csv.Count()}");
+            }
+        }
+
+        public object AddCsvLine(string line)
+        {
+            this._csv.Add(line);
+            return new
+            {
+                state = true,
+                message = "line successfully added!"
+            };
+        }
+
+        public List<string> searchSubstringInCsv(string subStr = " ")
+        {
+            try
+            {
+                List<string> result = new List<string>();
+
+                foreach (string str in this._csv)
+                {
+
+                    // search subStr in every line
+                    if (str.Contains(subStr))
+                    {
+                        result.Add(str);
+                    }
+                }
+
+                return result;
+            }catch(Exception err)
+            {
+                throw new Exception($"Something went wrong! We're working on it.");
+            }
+        }
+
+        public object getIndexOfSearch(string subStr)
+        {
+            List<string> result = new List<string>();
+            var indexOfres = 0;
+
+            foreach (string str in this._csv)
+            {
+                // search subStr in every line and note the index
+                if (str.Contains(subStr))
+                {
+                    result.Add(str);
+                    break;
+                }
+                indexOfres++;
+            }
+
+
+            return new
+            {
+                indexOf = indexOfres,
+                subStr,
+                result
+            };
+        }
+
+        public List<string> getDataBetween(string firstSub, string secondSub)
+        {
+            List<string> _res = new List<string>();
+
+            bool isBetween = false;
+
+            foreach (string str in this._csv)
+            {
+                if (str.Contains(firstSub))
+                {
+                    _res.Add(str);
+                    isBetween = true;
+                }
+            }
+            
+
+            return _res;
+        }
+    }
+}
---
+++ testapi/Program.cs
@@ -0,0 +1,78 @@
+using testapi;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Docs
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// enable Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+});
+
+// Define your API routes
+app.MapGet("/", () => "Hello World!");
+
+CsvLoader loader = new CsvLoader("./csv/testdata.csv");
+
+app.MapGet("/csv/count", () =>
+{
+    return new { status = 200, data = loader.GetCsvLines().Count() };
+});
+app.MapGet("/csv/search/index/{index}", (int index) =>
+{
+    try
+    {
+        return new { status = 200, data = loader.GetCsvLine(index) };
+    }
+        catch (Exception err)
+    {
+        return new { status = 500, data = err.Message };
+    }
+});
+app.MapGet("/csv/search/substring/{substring}", (string substring) =>
+{
+    try
+    {
+        var res = loader.searchSubstringInCsv(substring);
+        return new { status = 200, data = new { substring, value = res }  };
+    }
+    catch (Exception err)
+    {
+        var res = new List<string>();
+
+        res.Add(err.Message);
+
+        return new { status = 500, data = new { substring, value = res } };
+    }
+});
+app.MapGet("/csv/search/datetime/{startDate}/{endDate}", (string startDate, string endDate) =>
+{
+    try
+    {
+        
+        
+        //return new { status = 200, data = new { substring, value = res } };
+    }
+    catch (Exception err)
+    {
+        var res = new List<string>();
+
+        res.Add(err.Message);
+
+        return new { status = 500, data = new { substring, value = res } };
+    }
+});
+
+app.Run();
